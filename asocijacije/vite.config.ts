6e840a06baf1e9b0fed7b46694a3bec40eef238f
ALEKSA BABIC <aleksababic192@gmail.com>
2025-10-03 00:08:23 +0200

    Dodat tailwindcss za stilizovanje aplikacije i lucide react za ikonice kao sto su početna, statistika, i igraj koje se prikazuju u Nav baru.

--- asocijacije/vite.config.ts
+++ asocijacije/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // dodat kao plugin 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+});
+
+
+//da se koristi  tailwind css u reactu ,automatski generisan s
