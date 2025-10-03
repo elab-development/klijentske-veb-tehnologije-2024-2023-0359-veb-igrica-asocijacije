6a2cfc766683a4dbbb5ccb035dd310ef2ce84b92
ALEKSA BABIC <aleksababic192@gmail.com>
2025-10-03 13:21:03 +0200

    Kreiranje templejta izgleda svih stranice i početne stranice.

---
+++ asocijacije/src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+
+
+export default function HomePage() {
+  const navigate = useNavigate();
+
+
+  return (
+    <section className='space-y-8'>
+      <div className='rounded-3xl shadow-xl p-8 md:p-12 bg-white'>
+        <span>test</span>
+      </div>
+
+      
+    </section>
+  );
+}
+
