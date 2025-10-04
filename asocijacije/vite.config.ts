456b88850e02c552dd5d5a19310447e44f2f0db5
ninamilanovic890 <ninamilanovic890@gmail.com>
2025-10-04 15:12:48 +0200

    Ažuriranje konfiguracije

--- asocijacije/vite.config.ts
+++ asocijacije/vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // dodat kao plugin 
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
-
-
-//da se koristi  tailwind css u reactu ,automatski generisan s
